b26c8d5de857a89a9cebf4054777e2b8937861db
Mark <isfellop@gmail.com>
2019-04-20 12:27:23 +0300

    Automatic commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name='vkcoin',
-    version='1.10',
+    version='1.11',
     author="crinny",
     author_email="isfellop@gmail.com",
     description="Враппер для платёжного API VK Coin",
