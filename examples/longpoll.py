9fc26acd1a6b084e815c1a23bb60656a3e99fa3b
crinny <42045258+crinny@users.noreply.github.com>
2019-04-22 18:08:27 +0300

    Create longpoll.py

---
+++ examples/longpoll.py
@@ -0,0 +1,25 @@
+import vkcoin
+
+merchant = vkcoin.VKCoinApi(user_id=123456789, key='xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')  # Ваш ID и ключ
+
+
+@merchant.lp_handler
+def payment_received(data):
+    """
+    При получении платежа будет запущена эта функция. Она может называться как угодно
+
+    :param data.to_id: Ваш ID ВКонтакте
+    :param data.id: ID платежа
+    :param data.created_at: Unix timestamp, время когда был совершён платёж
+    :param data.from_id: ID отправителя платежа
+    :param data.amount: Количество полученных VK Coin
+    """
+
+    user_id = data.to_id
+    amount = data.amount
+
+    print('Получен платёж на сумму {amount} от {user_id}'.format(amount=amount, user_id=user_id))
+    # Вместо print вы можете выполнить ваши действия
+
+
+merchant.longpoll_start()  # Запускаем LongPoll
