0f871cedfbef86070b9e12b938083eb74ec085f4
crinny <42045258+crinny@users.noreply.github.com>
2019-06-11 20:52:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name='vkcoin',
-    version='2.0.3',
+    version='2.0.4',
     author="crinny",
     author_email="isfellop@gmail.com",
     description="Враппер для платёжного API VK Coin",
